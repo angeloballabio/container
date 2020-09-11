6d9ce571b9629c423140974ac707a310d4131bcb
Angelo Ballabio <angeloballabio@gmail.com>
2020-09-11 21:29:55 +0200

    Preparazione pagina mandati

--- app/Http/Livewire/GeneraMandati.php
+++ app/Http/Livewire/GeneraMandati.php
@@ -3,14 +3,37 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Operazione;
+use App\Dogana;
+use Carbon\Carbon;
+
 
 class GeneraMandati extends Component
 {
     public $ordine_id = 0;
+    public $nome_dogana, $indirizzo_dogana, $cap_dogana, $luogo_dogana, $provincia_dogana, $numero_dogana,$pratica, $nome_dogana_arrivo, $indirizzo_dogana_arrivo, $numero_dogana_arrivo,$cap_dogana_arrivo,$luogo_dogana_arrivo, $provincia_dogana_arrivo;
+    public $oggi;
 
     public function mount($id)
     {
         $this->ordine_id = $id;
+        $operazione = Operazione::where('id','=',$id)->get()->first();
+        $dogana = Dogana::where('soprannome','=',$operazione->dogana_t1)->get()->first();
+        $dogana_arrivo = Dogana::where('soprannome','=',$operazione->dogana_sdoganamento)->get()->first();
+        $this->nome_dogana = $dogana->nome;
+        $this->indirizzo_dogana = $dogana->indirizzo;
+        $this->numero_dogana = $dogana->numero;
+        $this->cap_dogana = $dogana->cap;
+        $this->luogo_dogana = $dogana->luogo;
+        $this->provincia_dogana = $dogana->prvincia;
+        $this->oggi = now()->format('d/m/Y');
+        $this->pratica = $operazione->numero_pratica;
+        $this->nome_dogana_arrivo = $dogana_arrivo->nome;
+        $this->indirizzo_dogana_arrivo = $dogana_arrivo->indirizzo;
+        $this->numero_dogana_arrivo = $dogana->numero;
+        $this->cap_dogana_arrivo = $dogana_arrivo->cap;
+        $this->luogo_dogana_arrivo = $dogana_arrivo->luogo;
+        $this->provincia_dogana_arrivo = $dogana_arrivo->provincia;
     }
 
     public function render()
--- resources/views/livewire/genera-mandati.blade.php
+++ resources/views/livewire/genera-mandati.blade.php
@@ -1,3 +1,32 @@
 <div>
     <h1 class="text-center">Genera mandati</h1>
+    <div class="col-12 row d-flex justify-content-between">
+        <div class="col-gap-1 col-4" text-align: left">
+            <img class="img-thumbnail" src="/immagini/logo3.jpg" alt="logo" style="width: 500px;">
+            <p>Via della speranza, 135, 20131 Milano
+            <br>Tel. 12345678, 2387645 r.a.
+            </p>
+        </div>
+        <div class="col-4 col-span-5">
+            <div class="card" style="width: 25rem;">
+                <div class="card-body">
+                  <h5 class="card-title" style="font-size: 30px">Spettabile ditta</h5>
+                  <p class="card-text" style="font-size: 20px">{{ $nome_dogana }}<br>
+                     {{ $indirizzo_dogana }}, {{ $numero_dogana }}<br>
+                    {{ $cap_dogana }} {{ $luogo_dogana }} {{ $provincia_dogana }}
+                    </p>
+                </div>
+              </div>
+        </div>
+    </div>
+    <p style="font-size: 20px;" class="p-2">Milano, {{ $oggi }}</p>
+    <h6 class="text-center" style="font-size: 20px; font-style: "><u>Oggetto import da: Cina</u></h6>
+    <p style="font-size: 20px;">Pratica numero : {{ $pratica }}</p>
+    <div class="text-center">
+        <p style="font-size: 25px;">In allegato documentazione per emissione T1 diretto al nostro magazzino presso:</p>
+        <p style="font-size: 30px;"><b>{{ $nome_dogana_arrivo }}<br>
+        {{ $indirizzo_dogana_arrivo }} , {{ $numero_dogana_arrivo }}<br>
+        {{ $cap_dogana_arrivo }} , {{ $luogo_dogana_arrivo }} , {{ $provincia_dogana_arrivo }}</b></p>
+    </div>
+
 </div>
